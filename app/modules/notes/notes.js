6e63c985df00f09f10d23145cbc231ce1548e513
Bert Maurau <bert.maurau@live.be>
2017-12-11 02:43:19 +0100

    added favourites selector

--- app/modules/notes/notes.js
+++ app/modules/notes/notes.js
@@ -43,6 +43,7 @@ angular.module('notes.notes', ['ngRoute'])
                 due_date: "",
                 closed: 1,
                 group: 0,
+                favourite: 0,
                 date_created: "",
                 date_updated: ""
             }, {
@@ -53,6 +54,7 @@ angular.module('notes.notes', ['ngRoute'])
                 due_date: "",
                 closed: 0,
                 group: 0,
+                favourite: 0,
                 date_created: "",
                 date_updated: ""
             }, {
@@ -63,6 +65,7 @@ angular.module('notes.notes', ['ngRoute'])
                 due_date: "",
                 closed: 0,
                 group: 0,
+                favourite: 1,
                 date_created: "",
                 date_updated: ""
             }]
